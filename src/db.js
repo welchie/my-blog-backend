41d2fb960c696a4d42b92743436e1b47a7fa3d34
welchie <chris@weewelchie.org>
2023-10-20 16:11:28 +0100

    Updated after accidentally deleting the project

--- src/db.js
+++ src/db.js
@@ -2,7 +2,8 @@ import { MongoClient } from "mongodb";
 let db;
 
 async function connectToDb(cb) {
-    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.jd2si1y.mongodb.net/?retryWrites=true&w=majority`); 
+   const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.jd2si1y.mongodb.net/?retryWrites=true&w=majority`); 
+   //const client = new MongoClient(`mongodb+srv://welchie99:IMqj9KIR2S5lg9zw@cluster0.upiopfk.mongodb.net/?retryWrites=true&w=majority`); 
     await client.connect();
 
     db = client.db('react-blog-db');
